1a029afd305199bca9a35f5a351d50c2d96bc1cc
Your Name <your.email@example.com>
2025-07-04 21:37:11 +0800

    Update FBMS project 2025-07-04 21:37

---
+++ .vite/deps/browser-K5OAUXD6.js
@@ -0,0 +1,18 @@
+import {
+  Headers,
+  Request,
+  Response,
+  browser_default,
+  fetch,
+  init_browser
+} from "./chunk-ZG2F6KTF.js";
+import "./chunk-EWTE5DHJ.js";
+init_browser();
+export {
+  Headers,
+  Request,
+  Response,
+  browser_default as default,
+  fetch
+};
+//# sourceMappingURL=browser-K5OAUXD6.js.map
